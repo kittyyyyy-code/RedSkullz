d60cc7ac4098f56864541f0b9d2afce24029e5ed
Kittyy <asherwhitford0@gmail.com>
2025-10-20 04:07:17 -0700

    Delete firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyC1COD1mt8I7u1qkVeE11yFE7ckyDqaN9A",
-  authDomain: "hexagon-c416d.firebaseapp.com",
-  projectId: "hexagon-c416d",
-  storageBucket: "hexagon-c416d.firebasestorage.app",
-  messagingSenderId: "243332053389",
-  appId: "1:243332053389:web:7eb2a7efc8c48a7c8f25c4"
-};
